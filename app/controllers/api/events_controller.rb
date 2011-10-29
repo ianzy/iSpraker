6a87cd4b90a4f2bea27c472b075f65df22f92201
Xianheng Ma <xianheng.ma@gmail.com>
2011-10-29 00:42:10 -0700

    + users, events location params

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,3 +1,19 @@
 class Api::EventsController < Api::BaseController
+
+    def collection
+    latitude = params[:latitude]
+    longitude = params[:longitude]
+
+    lat_lhs_range = latitude.to_f - 0.0007
+    lat_rhs_range = latitude.to_f + 0.0007
+
+    lng_lhs_range = longitude.to_f - 0.0007
+    lng_rhs_range = longitude.to_f + 0.0007
+
+    collection = super
+    collection.where(:lat => lat_lhs_range..lat_rhs_range,
+                     :lng => lng_lhs_range..lng_rhs_range ).select("name, description, url, lat, lng, address, event_time, created_at, updated_at")
+
+  end
   
 end
