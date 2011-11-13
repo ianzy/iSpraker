64e41a07c2b7b957de8d4f9e96bbc0178c206e4f
Yu Zhang <ian7zy@gmail.com>
2011-11-13 01:31:54 -0800

    temp

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,20 +1,20 @@
 class Api::UsersController < Api::BaseController
 
-  def collection
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-
-    lat_lhs_range = latitude.to_f - 0.007
-    lat_rhs_range = latitude.to_f + 0.007
-
-    lng_lhs_range = longitude.to_f - 0.007
-    lng_rhs_range = longitude.to_f + 0.007
-
-    collection = super
-    collection.where(:lat => lat_lhs_range..lat_rhs_range,
-                     :lng => lng_lhs_range..lng_rhs_range ).select("uid, screen_name, email, created_at, updated_at, lat, lng, profile_image_url, description, time_zone") if latitude && longitude
-    collection
-  end
+  # def collection
+  #   latitude = params[:latitude]
+  #   longitude = params[:longitude]
+  # 
+  #   lat_lhs_range = latitude.to_f - 0.007
+  #   lat_rhs_range = latitude.to_f + 0.007
+  # 
+  #   lng_lhs_range = longitude.to_f - 0.007
+  #   lng_rhs_range = longitude.to_f + 0.007
+  # 
+  #   collection = super
+  #   collection.where(:lat => lat_lhs_range..lat_rhs_range,
+  #                    :lng => lng_lhs_range..lng_rhs_range ).select("uid, screen_name, email, created_at, updated_at, lat, lng, profile_image_url, description, time_zone") if latitude && longitude
+  #   collection
+  # end
 
   def show
     @user = User.find(params[:id])
